b73840ef03da8f977d3dc7cc182d39c78f986fdc
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2024-09-03 18:20:13 -0400

    add allowed host

--- befarmayidsham/settings.py
+++ befarmayidsham/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mu&v)ud_7&qiycoaytg66qvff5)nyprap6fb^hh*&#w%d472v)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
